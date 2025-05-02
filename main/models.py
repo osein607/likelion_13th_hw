83fab9e3ebf992b4282c0c73d297eedd7fbc1368
osein607 <osein607@naver.com>
2025-05-02 14:58:59 +0900

    feat: Post 모델 CRUD 및 MEDIA 추가

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+  title = models.CharField(max_length=50)
+  writer = models.CharField(max_length=30)
+  content = models.TextField()
+  pub_date = models.DateTimeField()
+  mbti = models.CharField(max_length=50)
+  image = models.ImageField(upload_to="post/", blank=True, null=True)
+
+  def __str__(self):
+    return self.title
+
+  def summary(self):
+    return self.content[:20]
